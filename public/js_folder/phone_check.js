7294c094ee383833e98f422511e95518c8c9817a
AnnaHovhannisyan <hovhannisyan.ann179@gmail.com>
2022-03-17 02:02:49 +0400

    add a rule

--- public/js_folder/phone_check.js
+++ public/js_folder/phone_check.js
@@ -1,39 +1,49 @@
-window.onload=function() {
-let input=document.getElementById('dropdown');
-    let regexp= new RegExp('\D', 'gi');
+// eslint-disable-next-line no-undef
+window.onload = function() {
+// eslint-disable-next-line no-undef
+let input = document.getElementById('dropdown');
+     let regexp = /\D/gi;
+    
 
-    input.addEventListener('keydown',function (event) {
+    input.addEventListener('keydown', (event)=> {
 
     if(input.value.length === 0) {
         if (!(event.key === '0')) {
              event.preventDefault();
 
-
         }
     }
     if(input.value.length === 0 || input.value.length === 1) {
 
+        // eslint-disable-next-line no-undef
         document.getElementById("myDropdown").style.visibility = 'visible';
         if (!(event.key === '0')) {
              event.preventDefault();
 
         }
     }
-    let lists=document.querySelectorAll('li');
+    // eslint-disable-next-line no-undef
+    let lists = Array.from(document.querySelectorAll('li'));
+
+
+    // eslint-disable-next-line no-restricted-syntax
     for (let li of lists) {
-        li.onclick=function(){
+        // eslint-disable-next-line no-loop-func
+        li.onclick = function(){
             input.value += this.innerHTML;
-            let dr=document.getElementById('myDropdown');
+            
+            // eslint-disable-next-line no-undef
+            let dr = document.getElementById('myDropdown');
             dr.style.visibility='hidden';
             input.focus()
         }
     }
     if(input.value.length > 0){
 
-        if(regexp.test(input.value)){
-            event.preventDefault();
-            input.value='';
-        }
+         if(regexp.test(input.value)){
+             event.preventDefault();
+             input.value='';
+         }
 
 
     }
--- public/js_folder/phone_check2.js
+++ public/js_folder/phone_check2.js
@@ -1,8 +1,11 @@
-window.onload=function() {
-    let regexp= new RegExp('\D', 'gi');
-    let input2=document.getElementById('dropdown1');
+// eslint-disable-next-line no-undef
+window.onload = function(){
+     let regexp = /\D/gi;
+    
+    // eslint-disable-next-line no-undef
+    let input2 = document.getElementById('dropdown1');
 
-    input2.addEventListener('keydown',function (event) {
+    input2.addEventListener('keydown',(event)=> {
 
 
         if(input2.value.length === 0) {
@@ -13,30 +16,36 @@ window.onload=function() {
         }
         if(input2.value.length === 0 || input2.value.length === 1) {
 
+            // eslint-disable-next-line no-undef
             document.getElementById("myDropdown1").style.visibility = 'visible';
             if (!(event.key === '0')) {
                 event.preventDefault();
 
             }
         }
-        let lists=document.querySelectorAll('li');
+        // eslint-disable-next-line no-undef
+        let lists = Array.from(document.querySelectorAll('li'));
+       
+        // eslint-disable-next-line no-restricted-syntax
         for (let li of lists) {
-            li.onclick=function(){
+            // eslint-disable-next-line no-loop-func
+            li.onclick = function(){
                 input2.value += this.innerHTML;
-                let dr=document.getElementById('myDropdown1');
+                // eslint-disable-next-line no-undef
+                let dr = document.getElementById('myDropdown1');
                 dr.style.visibility='hidden';
                 input2.focus()
             }
         }
 
-        if(input2.value.length > 0){
+         if(input2.value.length > 0){
 
-            if(regexp.test(input2.value)){
+             if(regexp.test(input2.value)){
                 event.preventDefault();
-                input2.value='';
-            }
+                 input2.value='';
+             }
 
-        }
+         }
 
     });
 
